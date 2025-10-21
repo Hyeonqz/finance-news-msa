c7e6748f56aeeec5a500c7dfa09b9e39c059b497
hkjin <wlsgusrb78@naver.com>
2025-10-21 12:42:03 +0900

    :art: Enhance configuration management: connected config-server

--- infrastructures/config-server/src/main/java/io/github/hyeonqz/configserver/ConfigServerApplication.java
+++ infrastructures/config-server/src/main/java/io/github/hyeonqz/configserver/ConfigServerApplication.java
@@ -2,7 +2,13 @@ package io.github.hyeonqz.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Spring Cloud Config Server Application <br>
+ * MSA 환경에서 중앙 집중식 설정 관리를 제공하는 Config Server
+ */
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
