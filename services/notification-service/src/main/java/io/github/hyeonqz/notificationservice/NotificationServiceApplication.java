c5fbd7389d670aa9abf6e05802c2916f84a53ef7
hkjin <wlsgusrb78@naver.com>
2025-10-28 23:21:46 +0900

    :sparkles: Implement API Gateway with Spring Cloud Gateway and Eureka Integration

--- services/notification-service/src/main/java/io/github/hyeonqz/notificationservice/NotificationServiceApplication.java
+++ services/notification-service/src/main/java/io/github/hyeonqz/notificationservice/NotificationServiceApplication.java
@@ -2,7 +2,11 @@ package io.github.hyeonqz.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NotificationServiceApplication {
 
